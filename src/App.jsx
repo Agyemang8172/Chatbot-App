60cd989c0303b06a2337410ce54caf3563b92f2a
Agyemang8172 <godfredagyemang31052005@gmail.com>
2025-10-08 20:01:04 +0000

    Changes in api implementation

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,55 @@ import ChatWindow from './Components/ChatWindow'
 import ChatInput from './Components/ChatInput'
 
 
+
 function App() {
   const [messages,setMessages] = useState([]);
-
-  const handleSendMessage = async (text)  => {
-   const userMessage =  { id:Date.now(),text,sender:'user'};
-   setMessages(prev => [...prev,userMessage])
+  const [isLoading,setIsLoading] = useState(false);
 
  
 
-  }
+  const handleSendMessage = async (text)  => {
+   if (isLoading)  return;
 
+   const userMessage =  { id:Date.now(),text,sender:'user'};
+   setMessages(prev => [...prev,userMessage])
+   setIsLoading(true)
+    
+
+try{
+     const response = await fetch('http://localhost:3001/chat', {
+      method:'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ message:text })
+     })
+     
+  
+     if(!response.ok) {
+        throw new Error('Network responses was not ok.')
+      }
+
+
+        const data = await response.json();
+        const aiResponse = data.text
+
+        const aiMessage = {  id:Date.now() + 1, text: aiResponse, sender:"ai"};
+        setMessages((prev) => [...prev,aiMessage])
+       } 
+
+
+      catch (error) { 
+           console.error ('Failed to get responses from an API:'
+          , error) ;
+     const errorMessage = {
+        id : Date.now() + 1,
+        text: "Sorry I am unable to respond right now",
+        sender: 'ai'
+       };
+       setMessages((prev) => [...prev,errorMessage])
+      } finally{
+           setIsLoading(false);
+      }
+};
     const handleSuggestionClick= (suggestion) => {
      handleSendMessage(suggestion)
    }
@@ -37,7 +75,7 @@ function App() {
   </div>
   </div>
    
-  );
+  );  
 };
 
 export default App
