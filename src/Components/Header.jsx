b8530379b3a09536eb20ad85147fb5c43a6d045d
Agyemang8172 <godfredagyemang31052005@gmail.com>
2025-10-11 09:59:06 +0000

    Changing logo path

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <header className="mt-20 flex flex-col justify-center items-center space-y-8">
          <div>
-            <img src='src/assets/Logo.png'  />
+            <img src={Logo}  alt='logo'  />
          </div>
        <p className='mb-10'>Ask Our AI Anything</p>
     </header>
